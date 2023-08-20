c757f42290055d5c8d65e637f1bb22a2516513bb
zanzimi <pavlobajduza@gmail.com>
2023-08-20 21:45:12 +0300

    a1.2

--- src/main/java/com/zanz/zanz_mod/block/ModBlocks.java
+++ src/main/java/com/zanz/zanz_mod/block/ModBlocks.java
@@ -18,15 +18,26 @@ import java.util.Properties;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS
-            = DeferredRegister.create(ForgeRegistries.BLOCKS, ZanzMod.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ZanzMod.MOD_ID);
 
-    public static final RegistryObject<Block> CITRINE_ORE = registerBlock("citrine_ore",
-            () -> new Block(AbstractBlock.Properties.create(Material.ROCK)
-                    .harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
-    public static final RegistryObject<Block> CITRINE_BLOCK = registerBlock("citrine_block",
-            () -> new Block(AbstractBlock.Properties.create(Material.IRON)
-                    .harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> AMBER_ORE = registerBlock("amber_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> AMBER_BLOCK = registerBlock("amber_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> AMETHYST_ORE = registerBlock("amethyst_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> AMETHYST_BLOCK = registerBlock("amethyst_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> CARNATION_ORE = registerBlock("carnation_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> CARNATION_BLOCK = registerBlock("carnation_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> CITRINE_ORE = registerBlock("citrine_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> CITRINE_BLOCK = registerBlock("citrine_block", () -> new Block(AbstractBlock.Properties.create(Material.IRON).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> RAUCHTOPAZ_ORE = registerBlock("rauchtopaz_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> RAUCHTOPAZ_BLOCK = registerBlock("rauchtopaz_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> RUBY_ORE = registerBlock("ruby_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> RUBY_BLOCK = registerBlock("ruby_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> SAPHIRE_ORE = registerBlock("saphire_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> SAPHIRE_BLOCK = registerBlock("saphire_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> SLITTHER_BLOCK = registerBlock("slitther_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+
+    public static final RegistryObject<Block> TOPAZ_ORE = registerBlock("topaz_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
+    public static final RegistryObject<Block> TOPAZ_BLOCK = registerBlock("topaz_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5f)));
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
@@ -35,8 +46,7 @@ public class ModBlocks {
     }
 
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                        new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/com/zanz/zanz_mod/item/ModItems.java
+++ src/main/java/com/zanz/zanz_mod/item/ModItems.java
@@ -1,8 +1,8 @@
 package com.zanz.zanz_mod.item;
 
 import com.zanz.zanz_mod.ZanzMod;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,8 +12,37 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ZanzMod.MOD_ID);
 
-    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
-            () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> AMBER = ITEMS.register("amber", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> AMETHYST = ITEMS.register("amethyst", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> CARNATION = ITEMS.register("carnation", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> RAUCHTOPAZ = ITEMS.register("rauchtopaz", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SAPHIRE = ITEMS.register("saphire", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER = ITEMS.register("slitther", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz", () -> new Item(new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+
+    public static final RegistryObject<Item> NELITTHER_SWORD = ITEMS.register("nelitther_sword", () -> new SwordItem(ModItemTier.NELITTHER, 9, -2.0f,new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_PICKAXE = ITEMS.register("nelitther_pickaxe", () -> new PickaxeItem(ModItemTier.NELITTHER, 5, -1f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_SHOVEL = ITEMS.register("nelitther_shovel", () -> new ShovelItem(ModItemTier.NELITTHER, 3, -1f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_AXE = ITEMS.register("nelitther_axe", () -> new AxeItem(ModItemTier.NELITTHER, 11, -1f,new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_HOE = ITEMS.register("nelitther_hoe", () -> new HoeItem(ModItemTier.NELITTHER, 0, -1f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_BOOTS = ITEMS.register("nelitther_boots", () -> new ArmorItem(ModArmorMaterial.NELITTHER, EquipmentSlotType.FEET, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_LEGGINGS = ITEMS.register("nelitther_leggings", () -> new ArmorItem(ModArmorMaterial.NELITTHER, EquipmentSlotType.LEGS, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_CHESTPLATE = ITEMS.register("nelitther_chestplate", () -> new ArmorItem(ModArmorMaterial.NELITTHER, EquipmentSlotType.CHEST, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> NELITTHER_HELMET = ITEMS.register("nelitther_helmet", () -> new ArmorItem(ModArmorMaterial.NELITTHER, EquipmentSlotType.HEAD, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+
+    public static final RegistryObject<Item> SLITTHER_SWORD = ITEMS.register("slitther_sword", () -> new SwordItem(ModItemTier.SLITTHER, 14, -1.8f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_PICKAXE = ITEMS.register("slitther_pickaxe", () -> new PickaxeItem(ModItemTier.SLITTHER, 6, -1f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_SHOVEL = ITEMS.register("slitther_shovel", () -> new ShovelItem(ModItemTier.SLITTHER, 4, -1f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_AXE = ITEMS.register("slitther_axe", () -> new AxeItem(ModItemTier.SLITTHER, 17, -1f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_HOE = ITEMS.register("slitther_hoe", () -> new HoeItem(ModItemTier.SLITTHER, 0, -1f, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_BOOTS = ITEMS.register("slitther_boots", () -> new ArmorItem(ModArmorMaterial.SLITTHER, EquipmentSlotType.FEET, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_LEGGINGS = ITEMS.register("slitther_leggings", () -> new ArmorItem(ModArmorMaterial.SLITTHER, EquipmentSlotType.LEGS, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_CHESTPLATE = ITEMS.register("slitther_chestplate", () -> new ArmorItem(ModArmorMaterial.SLITTHER, EquipmentSlotType.CHEST, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+    public static final RegistryObject<Item> SLITTHER_HELMET = ITEMS.register("slitther_helmet", () -> new ArmorItem(ModArmorMaterial.SLITTHER, EquipmentSlotType.HEAD, new Item.Properties().group(ModItemTab.MOD_MATERIAL_TAB)));
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
